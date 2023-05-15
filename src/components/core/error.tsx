9c7e66531aa90586c06ea4f9f1e99ed691f4dbc9
TheMysterys <the-mystery@outlook.com>
2023-05-16 10:00:37 +1200

    error display component

---
+++ src/components/core/error.tsx
@@ -0,0 +1,12 @@
+export default function Error({ message }: { message: string }) {
+	return (
+		<div className="col-span-full flex flex-row rounded-lg">
+			<p className="w-24 h-20 flex justify-center items-center bg-blue-400 uppercase rounded-l-lg font-semibold">
+				Error
+			</p>
+			<p className="w-[calc(100%-100px)] h-20 flex justify-center items-center text-center ">
+				{message}
+			</p>
+		</div>
+	);
+}
