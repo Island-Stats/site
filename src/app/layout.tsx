76166cb706b6829488e7d27a34c25841372ffce1
TheMysterys <the-mystery@outlook.com>
2023-05-16 10:00:08 +1200

    added base metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,24 @@
+import { Metadata } from "next";
 import "./globals.css";
 
-export const metadata = {
-	title: "Island Stats",
+export const metadata: Metadata = {
+	title: {
+		template: "%s | Island Stats",
+		default: "Island Stats",
+	},
 	description: "MCC Island stats, data, and more!",
+	themeColor: "#60a5fa",
+	openGraph: {
+		type: "website",
+		title: "Island Stats",
+		description: "MCC Island stats, data, and more!",
+		url: "https://island.themysterys.com/",
+	},
+	twitter: {
+		card: "summary",
+		title: "Island Stats",
+		description: "MCC Island stats, data, and more!",
+	},
 };
 
 export default function RootLayout({
