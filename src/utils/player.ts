0bf939bbb0fd06773f04b39f8553b70d9c8004dd
TheMysterys <the-mystery@outlook.com>
2023-05-16 10:02:00 +1200

    Player data utility methods

---
+++ src/utils/player.ts
@@ -0,0 +1,45 @@
+export enum Size {
+	small = 32,
+	medium = 64,
+	large = 128,
+	full = 512,
+}
+
+export interface MojangProfile {
+	id: string;
+	name: string;
+}
+
+const usernameURL = "https://mojang-api.svc.noxcrew.online/username/";
+const uuidURL = "https://mojang-api.svc.noxcrew.online/uuid/";
+
+/**
+ * Get the URL of a player's head
+ *
+ * @param uuid UUID of the player
+ * @param size Size of player head (default: Medium | 64px)
+ * @returns URL of player head
+ */
+export function getPlayerHead(uuid: string, size: Size = Size.medium) {
+	return `https://crafatar.com/avatars/${uuid}?size=${size}&overlay`;
+}
+
+/**
+ * Get the mojang profile of a player
+ *
+ * @param data UUID or Username of player
+ * @returns Mojang Profile of player
+ */
+export async function getMojangProfile(
+	data: string
+): Promise<MojangProfile | undefined> {
+	try {
+		let url = uuidURL;
+		if (data.length > 16) url = usernameURL;
+		const response = await fetch(url + data);
+		if (!response.ok) return undefined;
+		return await response.json();
+	} catch (ignored) {
+		return;
+	}
+}
