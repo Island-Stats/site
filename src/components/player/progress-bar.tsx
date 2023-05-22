810cee483dee2c357e75f072c888bf38fe29313e
TheMysterys <the-mystery@outlook.com>
2023-05-23 01:19:11 +1200

    Created progress bars

---
+++ src/components/player/progress-bar.tsx
@@ -0,0 +1,81 @@
+"use client";
+import Tippy from "@tippyjs/react";
+import { ReactElement, useState } from "react";
+
+export default function ProgressBar({
+	title,
+	level,
+	current,
+	max,
+	suffix,
+	icon,
+	iconHover,
+}: {
+	title: string;
+	level?: number;
+	current: number;
+	max: number;
+	suffix: string;
+	icon?: string;
+	iconHover?: string | ReactElement;
+}) {
+	const [hover, setHover] = useState(false);
+	return (
+		<div className="relative h-9 text-base">
+			{icon && iconHover && (
+				<Tippy content={iconHover} allowHTML={true} >
+					<div className="relative w-9 h-9 bg-sky-500 icon-shadow rounded-full z-10">
+						<div
+							className="absolute top-2.5 left-2.5 w-4 h-4 z-10 pixelated"
+							style={{ backgroundImage: `url('/images/${icon}')` }}
+						></div>
+					</div>
+				</Tippy>
+			) || icon && (
+				<div className="relative w-9 h-9 bg-sky-500 icon-shadow rounded-full z-10">
+					<div
+						className="absolute top-2.5 left-2.5 w-4 h-4 z-10 pixelated"
+						style={{ backgroundImage: `url('/images/${icon}')` }}
+					></div>
+				</div>
+				)}
+			<div
+				className={`absolute ${
+					icon ? "left-10" : "left-5"
+				} top-0 font-bold text-sm`}
+			>
+				{title}
+				{level != undefined && <span className="text-neutral-400"> {level}</span>}
+			</div>
+			<div
+				className={`absolute left-4 bottom-0 pl-4 right-0 h-4 bg-neutral-600 ${
+					icon ? "rounded-r-md" : "rounded-md"
+				}`}
+			>
+				<div
+					className={`absolute left-0 top-0 bottom-0 bg-sky-500 ${
+						icon ? "rounded-r-md" : "rounded-md"
+					} text-center`}
+					style={{ width: `calc((100%-20px)*${(current / max).toFixed(4)})` }}
+				></div>
+				<div onMouseOver={() => setHover(true)} onMouseOut={() => setHover(false)} className="absolute left-5 right-0 top-0 bottom-0 text-center font-semibold text-xs">
+					{hover ? `${current.toLocaleString()} / ${max.toLocaleString()}` : `${
+						// Shorten numbers to thousands if greater than 1,000 (10k) or millions if greater than 1,000,000 (10m)
+						current >= 1000000
+							? `${(current / 1000000).toFixed(1)}M`
+							: current >= 1000
+							? `${(current / 1000).toFixed(0)}K`
+							: current
+					} / ${
+						// Shorten numbers to thousands if greater than 1,000 (10k) or millions if greater than 1,000,000 (10m)
+						max >= 1000000
+							? `${(max / 1000000).toFixed(1)}M`
+							: max >= 1000
+							? `${(max / 1000).toFixed(0)}K`
+							: max
+					}`} {suffix}
+				</div>
+			</div>
+		</div>
+	);
+}
