034e90f17080b14cdc6789a59a3f87565941e0af
TheMysterys <the-mystery@outlook.com>
2023-05-23 14:11:50 +1200

    Switched back to using an object

--- src/components/player/rank.tsx
+++ src/components/player/rank.tsx
@@ -1,27 +1,26 @@
 import { type } from "os";
 
-export enum Ranks {
-	noxcrew = "Noxcrew",
-	admin = "Admin",
-	mod = "Mod",
-	creator = "Creator",
-	contestant = "Contestant",
-	vip = "VIP",
-	gc_royal = "Grand Champ Royale",
-	grand_champ = "Grand Champ",
-	champ = "Champ",
-	player = 0,
-}
+export const Ranks = {
+	noxcrew: "Noxcrew",
+	admin: "Admin",
+	mod: "Mod",
+	creator: "Creator",
+	contestant: "Contestant",
+	vip: "VIP",
+	gc_royal: "Grand Champ Royale",
+	grand_champ: "Grand Champ",
+	champ: "Champ",
+	player: "Player",
+};
 
-export default function Rank({ rank }: { rank: Ranks }) {
+export default function Rank({ rank }: { rank: string }) {
 	if (rank == Ranks.player) return null;
-	const rankKey = Object.keys(Ranks)[Object.values(Ranks).indexOf(rank)]
 	return (
 		<div
 			className="mcc-colors flex mt-1 px-4 rounded-full font-bold text-lg justify-center items-center"
-			style={{ backgroundColor: `var(--${rankKey})` }}
+			style={{ backgroundColor: `var(--${rank})` }}
 		>
-			{rank}{" "}
+			{Ranks[rank as keyof typeof Ranks]}{" "}
 		</div>
 	);
 }
