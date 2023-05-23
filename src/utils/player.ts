420961facfc3d40ddeeafbb4b36d3e2a7558a861
TheMysterys <the-mystery@outlook.com>
2023-05-23 14:13:10 +1200

    Added fetching player data
    No cache for now but in future stats will be changed for 5 mins

--- src/utils/player.ts
+++ src/utils/player.ts
@@ -43,3 +43,65 @@ export async function getMojangProfile(
 		return;
 	}
 }
+
+/**
+ * Player Data type
+ */
+export type PlayerData = {
+	uuid: string;
+	rank: string;
+	data: {
+		sky_battle: {
+			survival_first_place: number;
+			survival_top_three: number;
+			survival_top_five: number;
+			survival_top_eight: number;
+			survival_top_fifty_percent: number;
+			players_killed: number;
+			games_played: number;
+		};
+		battle_box: {
+			team_first_place: number;
+			team_second_place: number;
+			team_third_place: number;
+			team_fourth_place: number;
+			team_rounds_won: number;
+			players_killed: number;
+			games_played: number;
+		};
+		tgttos: {
+			first_place: number;
+			top_three: number;
+			top_five: number;
+			top_eight: number;
+			top_fifty_percent: number;
+			round_first_place: number;
+			round_top_three: number;
+			round_top_five: number;
+			round_top_eight: number;
+			round_top_fifty_percent: number;
+			chickens_punched: number;
+			games_played: number;
+		};
+		hole_in_the_wall: {
+			first_place: number;
+			top_three: number;
+			top_five: number;
+			top_eight: number;
+			top_fifty_percent: number;
+			walls_dodged: number;
+			games_played: number;
+		};
+		games_played: number;
+	};
+};
+
+export async function getPlayerData(id: string): Promise<PlayerData | undefined> {
+	const response = await fetch(`http://localhost:3000/files/${id}.json`, {
+		next: {
+			revalidate: 0, // Cache for 5 minutes
+		},
+	});
+	if (!response.ok) return undefined;
+	return (await response.json());
+}
