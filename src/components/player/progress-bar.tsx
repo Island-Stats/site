d2d9c92a6ec05921d328a01507a431fe73a0fdfd
TheMysterys <the-mystery@outlook.com>
2023-05-28 16:03:31 +1200

    Made progress bar bigger

--- src/components/player/progress-bar.tsx
+++ src/components/player/progress-bar.tsx
@@ -21,34 +21,34 @@ export default function ProgressBar({
 }) {
 	const [hover, setHover] = useState(false);
 	return (
-		<div className="relative h-9 text-base">
+		<div className="relative h-12 text-base">
 			{icon && iconHover && (
 				<Tippy content={iconHover} allowHTML={true} >
-					<div className="relative w-9 h-9 bg-sky-500 icon-shadow rounded-full z-10">
+					<div className="relative w-12 h-12 bg-sky-500 icon-shadow rounded-full z-10">
 						<div
-							className="absolute top-2.5 left-2.5 w-4 h-4 z-10 pixelated"
+							className="absolute top-4 left-4 w-4 h-4 scale-[2] z-10 pixelated"
 							style={{ backgroundImage: `url('/images/${icon}')` }}
 						></div>
 					</div>
 				</Tippy>
 			) || icon && (
-				<div className="relative w-9 h-9 bg-sky-500 icon-shadow rounded-full z-10">
-					<div
-						className="absolute top-2.5 left-2.5 w-4 h-4 z-10 pixelated"
-						style={{ backgroundImage: `url('/images/${icon}')` }}
-					></div>
-				</div>
+				<div className="relative w-12 h-12 bg-sky-500 icon-shadow rounded-full z-10">
+						<div
+							className="absolute top-4 left-4 w-4 h-4 scale-[2] z-10 pixelated"
+							style={{ backgroundImage: `url('/images/${icon}')` }}
+						></div>
+					</div>
 				)}
 			<div
 				className={`absolute ${
-					icon ? "left-10" : "left-5"
+					icon ? "left-14" : "left-7"
 				} top-0 font-bold text-sm`}
 			>
 				{title}
 				{level != undefined && <span className="text-neutral-400"> {level}</span>}
 			</div>
 			<div
-				className={`absolute left-4 bottom-0 pl-4 right-0 h-4 bg-neutral-600 ${
+				className={`absolute left-6 bottom-0 pl-4 right-0 h-6 bg-neutral-600 ${
 					icon ? "rounded-r-md" : "rounded-md"
 				}`}
 			>
@@ -58,7 +58,7 @@ export default function ProgressBar({
 					} text-center`}
 					style={{ width: `calc((100%-20px)*${(current / max).toFixed(4)})` }}
 				></div>
-				<div onMouseOver={() => setHover(true)} onMouseOut={() => setHover(false)} className="absolute left-5 right-0 top-0 bottom-0 text-center font-semibold text-xs">
+				<div onMouseOver={() => setHover(true)} onMouseOut={() => setHover(false)} className="absolute left-5 right-0 top-0 bottom-0 text-center font-semibold text-sm">
 					{hover ? `${current.toLocaleString()} / ${max.toLocaleString()}` : `${
 						// Shorten numbers to thousands if greater than 1,000 (10k) or millions if greater than 1,000,000 (10m)
 						current >= 1000000
