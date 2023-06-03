29c577fa587df1ae1da885e8fbd4008f6a6d7535
TheMysterys <the-mystery@outlook.com>
2023-06-03 22:12:45 +1200

    Center error text

--- src/components/core/error.tsx
+++ src/components/core/error.tsx
@@ -4,7 +4,7 @@ export default function Error({ message }: { message: string }) {
 			<p className="w-24 h-20 flex justify-center items-center bg-blue-400 uppercase rounded-l-lg font-semibold">
 				Error
 			</p>
-			<p className="w-[calc(100%-100px)] h-20 flex justify-center items-center text-center ">
+			<p className="w-[calc(100%-200px)] h-20 flex justify-center items-center text-center ">
 				{message}
 			</p>
 		</div>
