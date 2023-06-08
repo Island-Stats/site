9e832fe589e46ea0a3dc4e3144d2ca564f2b88f2
TheMysterys <the-mystery@outlook.com>
2023-06-08 19:31:03 +1200

    about starting code

--- src/components/core/nab-bar.tsx
+++ src/components/core/nab-bar.tsx
@@ -1,11 +1,18 @@
+"use client";
 import Link from "next/link";
 
 export default function NavBar() {
 	return (
-		<header className="absolute top-0 left-0 right-0 h-12 bg-neutral-800 flex flex-row items-center z-50">
+		<header className="fixed top-0 left-0 right-0 h-12 bg-neutral-800 flex flex-row items-center z-50">
 			<Link href="/" className="mx-2 font-semibold">
 				Island Stats
 			</Link>
+			<button className="expander" aria-expanded={false} id="info-button">
+				About
+			</button>
+			<div id="info-box" className="expandable">
+				<p>Island Stats is a website to display </p>
+			</div>
 		</header>
 	);
 }
