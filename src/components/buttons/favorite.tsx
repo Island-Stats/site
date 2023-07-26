604867d5a70a03ce7fa3337eae885a50e73b1ba8
TheMysterys <the-mystery@outlook.com>
2023-07-26 16:39:23 +1200

    Setup with new tooltip system

--- src/components/buttons/favorite.tsx
+++ src/components/buttons/favorite.tsx
@@ -1,12 +1,31 @@
 "use client";
-import Tippy from "@tippyjs/react";
+import {
+	FloatingArrow,
+	arrow,
+	autoUpdate,
+	offset,
+	useFloating,
+} from "@floating-ui/react";
 import { getCookie, setCookie } from "cookies-next";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function FavoriteButton({ uuid }: { uuid: string }) {
 	const [checked, setChecked] = useState(false);
 	const [visible, setVisible] = useState(false);
+
+	const arrowRef = useRef(null);
+	const { refs, floatingStyles, context } = useFloating({
+		open: visible,
+		onOpenChange: setVisible,
+		middleware: [
+			offset(5),
+			arrow({
+				element: arrowRef,
+			}),
+		],
+		whileElementsMounted: autoUpdate,
+	});
 
 	const clickFavorite = (
 		_event: React.MouseEvent<HTMLButtonElement, MouseEvent>
@@ -43,40 +62,39 @@ export default function FavoriteButton({ uuid }: { uuid: string }) {
 	}, [uuid]);
 
 	return (
-		<Tippy
+		<button
+			ref={refs.setReference}
+			role="checkbox"
+			aria-checked={!checked}
+			onClick={clickFavorite}
+			className="group bg-blue-400 mt-4 mr-2 p-1 rounded-full"
+		>
+			<Image
+				src={"/images/icons/favorite.png"}
+				alt="Favorite"
+				title="Favorite"
+				width={16}
+				height={16}
+				className="w-5 h-5 pixelated group-aria-checked:grayscale"
+			/>
+			{visible && (
+				<div ref={refs.setFloating} style={floatingStyles} className="z-20">
+					<div className="bg-neutral-800 p-2 rounded-md text-center whitespace-nowrap">
+						{checked ? "Added to favorites" : "Removed from favorites"}
+					</div>
+					<FloatingArrow
+						ref={arrowRef}
+						context={context}
+						className="fill-sky-500"
+					/>
+				</div>
+			)}
+			{/* <Tippy
 			visible={visible}
 			content={checked ? "Added to favorites" : "Removed from favorites"}
 		>
-			<button
-				role="checkbox"
-				aria-checked={!checked}
-				onClick={clickFavorite}
-				className="group bg-blue-400 mt-4 mr-2 p-1 rounded-full"
-			>
-				{/* <svg viewBox="0 0 24 24" className="w-5 h-5">
-					<title>Favorite</title>
-					<path
-						className="solid"
-						fill="white"
-						visibility={checked ? "visible" : "hidden"}
-						d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"
-					></path>
-					<path
-						className="solid"
-						fill="white"
-						visibility={!checked ? "visible" : "hidden"}
-						d="M12,15.39L8.24,17.66L9.23,13.38L5.91,10.5L10.29,10.13L12,6.09L13.71,10.13L18.09,10.5L14.77,13.38L15.76,17.66M22,9.24L14.81,8.63L12,2L9.19,8.63L2,9.24L7.45,13.97L5.82,21L12,17.27L18.18,21L16.54,13.97L22,9.24Z"
-					></path>
-				</svg> */}
-				<Image
-					src={"/images/icons/favorite.png"}
-					alt="Favorite"
-					title="Favorite"
-					width={16}
-					height={16}
-					className="w-5 h-5 pixelated group-aria-checked:grayscale"
-				/>
-			</button>
-		</Tippy>
+			
+		</Tippy> */}
+		</button>
 	);
 }
