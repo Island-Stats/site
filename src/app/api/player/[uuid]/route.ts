ad0cb5391901ad28783bb4304c429ec5afb9f9bf
TheMysterys <the-mystery@outlook.com>
2023-08-08 11:19:11 +1200

    moved back to mongodb

--- src/app/api/player/[uuid]/route.ts
+++ src/app/api/player/[uuid]/route.ts
@@ -1,4 +1,4 @@
-import { Player, playerModel } from "@/utils/mongoose";
+import db from "@/utils/mongodb";
 import { getMojangProfile } from "@/utils/player";
 import { NextResponse } from "next/server";
 
@@ -14,68 +14,60 @@ export async function POST(
 
 	const game = searchParams.get("game");
 
-	let player = await playerModel.findOne({ uuid }).exec();
+	const playerCollection = db.collection("players");
+
+	let player = await playerCollection.findOne({ uuid });
+
+	console.log();
 
 	if (
-		player == null ||
-		Date.now() - player.last_modified! > revalidateTime * 1000 ||
+		(player == null ||
+			Date.now() - player.last_modified! > revalidateTime * 1000) &&
 		process.env.CACHE_ONLY != "true"
 	) {
 		const res = await fetch(`http://localhost:3000/files/${uuid}.json`);
 
 		if (res.status == 404) {
-			return NextResponse.json(
-				{ error: "Player not found" },
-				{ status: 404 }
-			);
+			return NextResponse.json({ error: "Player not found" }, { status: 404 });
 		}
 
 		const resJSON = await res.json();
 
-		if (player == null) {
-			if (resJSON.username == undefined) {
-				resJSON.username = (await getMojangProfile(resJSON.uuid))?.name;
-			}
-
-			player = new playerModel({
-				uuid: resJSON.uuid,
-				username: resJSON.username,
-				rank: resJSON.rank,
-				games: resJSON.games,
-				games_played: resJSON.games_played,
-				last_modified: Date.now(),
-			});
-		} else {
-			player.username = resJSON.username;
-			player.rank = resJSON.rank;
-			player.games = resJSON.games;
-			player.games_played = resJSON.games_played;
-			player.last_modified = Date.now();
+		if (resJSON.username == undefined) {
+			resJSON.username = (await getMojangProfile(resJSON.uuid))?.name;
 		}
 
-		await player.save();
+		player = resJSON;
+		player!.last_modified = Date.now();
+
+		const result = await playerCollection.updateOne(
+			{ uuid },
+			{ $set: player },
+			{ upsert: true }
+		);
+
+		if (result.upsertedId) {
+			player!._id = result.upsertedId;
+		}
+	}
+
+	if (player == null) {
+		if (process.env.CACHE_ONLY == "true") {
+			return NextResponse.json(
+				{ error: "Player not in cache" },
+				{ status: 404 }
+			);
+		} else {
+			return NextResponse.json({ error: "Player not found" }, { status: 404 });
+		}
 	}
 
 	if (game != "" && game != null) {
-		// Remove games that aren't the requested game but keep the games object
-
-		if (player.games[game] == undefined) {
-			return NextResponse.json(
-				{ error: "Game not found" },
-				{ status: 404 }
-			);
+		if (player!.games[game] == undefined) {
+			return NextResponse.json({ error: "Game not found" }, { status: 404 });
 		}
 
-		const json = {
-			uuid: player.uuid,
-			username: player.username,
-			rank: player.rank,
-			games: { [game]: player.games[game] },
-			games_played: player.games_played,
-			last_modified: player.last_modified,
-		};
-
-		return NextResponse.json(json);
+		player!.games = { [game]: player!.games[game] };
 	}
 
 	return NextResponse.json(player);
