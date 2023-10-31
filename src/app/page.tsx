9f2b5ed2687589f3ec823f6deee2daa0f7693b3b
TheMysterys <the-mystery@outlook.com>
2023-10-31 13:42:08 +1300

    Brought back notice board

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,24 @@
 import MainSearch from "@/components/core/main-search";
-//import NoticeBoard from "@/components/core/notice-board";
 import Favorites from "@/components/core/favorites";
 import Profiles from "@/components/core/profiles";
+import NoticeBoard from "@/components/core/notice-board";
 
 export default function Home() {
 	return (
 		<main className="grid">
 			<MainSearch />
-			{/* <NoticeBoard /> */}
-			<div
-				className="group col-span-full py-5 text-center"
-			>
+			<NoticeBoard />
+			<div className="group col-span-full py-5 px-2 text-center">
 				<p className="text-neutral-200 text-xl font-bold transition-colors">
 					Site preview
 				</p>
-				<p className="text-neutral-200">This is a preview of what Island Stats will hopefully be in the future once an MCC Island API is released to the public.</p>
-				<p className="text-neutral-200 font-semibold">Only the below profiles contain stats</p>
+				<p className="text-neutral-200">
+					This is a preview of what Island Stats will hopefully be in
+					the future once an MCC Island API is released to the public.
+				</p>
+				<p className="text-neutral-200 font-semibold">
+					Only the below profiles contain stats
+				</p>
 			</div>
 			<Favorites />
 			<Profiles />
